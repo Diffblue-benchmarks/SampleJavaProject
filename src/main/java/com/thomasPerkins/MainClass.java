8939e9bc945562b02445478b7fa0f448d3594d8d
Thomas Perkins <thomasperkins1123@gmail.com>
2019-12-10 10:21:59 +0000

    First commit

---
+++ src/main/java/com/thomasPerkins/MainClass.java
@@ -0,0 +1,16 @@
+package com.thomasPerkins;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println(addOne(1));
+		System.out.println(addS("run"));
+	}
+
+	public static int addOne(int n) {
+		return n + 1;
+	}
+
+	public static String addS(String s) {
+		return s + "s";
+	}
+}
